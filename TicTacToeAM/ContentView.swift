1a6115e58943eb0649767ec6fcabb4b691af6daa
jefferymason <jefferymason72@gmail.com>
2021-09-15 10:41:48 -0500

    Checking for winner functions created.

--- TicTacToeAM/ContentView.swift
+++ TicTacToeAM/ContentView.swift
@@ -21,9 +21,11 @@ struct ContentView: View {
 struct Home: View {
     
     // Number of moves we can make
-    @State var moves: [String] = Array(repeating: "", count: 9)
+    @State var moves : [String] = Array(repeating: "", count: 9)
     // To identify our current player
     @State var isPlaying = true
+    @State var gameOver = false
+    @State var msg = ""
     
     var body: some View {
         VStack {
@@ -71,6 +73,11 @@ struct Home: View {
             .padding(15)
             
         }
+        
+        .onChange(of: moves, perform: { value in
+            
+            checkWinner()
+        })
     }
     
     // Calculate width of grid
@@ -78,6 +85,35 @@ struct Home: View {
         let width = UIScreen.main.bounds.width - (30 + 30)
         
         return width / 3
+    }
+
+
+    func checkWinner() {
+    
+        if checkMoves(player: "X") {
+            
+            msg = "Player X Won!!!"
+            gameOver.toggle()
+        }
+        
+        else if checkMoves(player: "O") {
+            
+            msg = "Player O Won!!!"
+            gameOver.toggle()
+        }
+}
+
+    func checkMoves(player: String) -> Bool {
+    
+        for contestant in stride(from: 0, to: 9, by: 3) {
+            if moves[contestant] == player &&
+                moves[contestant+1] == player &&
+                moves[contestant+2] == player {
+            
+                return true
+        }
+    }
+    return false
     }
 }
 
