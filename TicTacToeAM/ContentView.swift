307f9a43d61fc20ef076e26c38340bca23e30371
jefferymason <jefferymason72@gmail.com>
2021-09-15 10:13:41 -0500

    3D effect works.

--- TicTacToeAM/ContentView.swift
+++ TicTacToeAM/ContentView.swift
@@ -45,8 +45,15 @@ struct Home: View {
                             .fontWeight(.heavy)
                             .foregroundColor(.white)
                     }
-                        .frame(width: getWidth(), height: getWidth())
-                        .cornerRadius(15)
+                    .frame(width: getWidth(), height: getWidth())
+                    .cornerRadius(15)
+                    .rotation3DEffect(
+                        .init(degrees: moves[index] != "" ? 180 : 0),
+                        axis: (x: 0.0, y: 1.0, z: 0.0),
+                        anchor: .center,
+                        anchorZ: 0.0,
+                        perspective: 1.0
+                    )
                     
                     .onTapGesture(perform: {
                         withAnimation(Animation.easeIn(duration: 0.5)) {
