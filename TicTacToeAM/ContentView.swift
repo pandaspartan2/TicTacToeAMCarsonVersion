cb27c6cbbbe51df3469c2020b0c425ca7bf227fc
jefferymason <jefferymason72@gmail.com>
2021-09-15 08:29:12 -0500

    Initial Commit

---
+++ TicTacToeAM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TicTacToeAM
+//
+//  Created by Jeff Mason on 9/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
